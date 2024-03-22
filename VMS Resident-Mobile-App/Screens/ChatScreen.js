f10bba8a38e1c8550efd96f42bcf3427dfac74a6
Okere1 <promiseokere958@gmail.com>
2024-03-22 08:27:10 +0100

    modified chat page

--- VMS Resident-Mobile-App/Screens/ChatScreen.js
+++ VMS Resident-Mobile-App/Screens/ChatScreen.js
@@ -4,7 +4,10 @@ import { Button, View } from "react-native";
 export default function ChatScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button onPress={() => navigation.navigate("Home")} title="Go to Home" />
+      <Button
+        onPress={() => navigation.navigate("Home")}
+        title="Navigate to Home"
+      />
     </View>
   );
 }
